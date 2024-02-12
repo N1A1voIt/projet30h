3f9a07136b3b2ca81988bd0d8f82a1d904795ab8
N1A1voIt <120253454+N1A1voIt@users.noreply.github.com>
2024-02-13 00:31:35 +0100

    getTotalReste

--- back/frontoffice/every-result.php
+++ back/frontoffice/every-result.php
@@ -1,10 +1,14 @@
 <?php
-$id_parcelle = $_POST['id_parcelle'];
 $date_debut = $_POST['date_debut'];
 $date_fin = $_POST['date_fin'];
 
 $poids_total_restant = getRestePoids($id_parcelle, $date_debut, $date_fin);
-$poids_total_cueilli = getPoidsCueillette($id_parcelle, $date_debut, $date_fin);
+
+/*
+ * TODO: getRestePoidsTotal
+ * */
+
+$poids_total_cueilli = getPoidsCueilletteTotal($date_debut, $date_fin);
 $ca = getCoutRevientRecolte($date_debut, $date_fin);
 
 $ret = array(
