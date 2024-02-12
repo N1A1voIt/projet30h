7efba9528263834513400e7af97111e4444ccfc2
benimaru73 <153352359+benimaru73@users.noreply.github.com>
2024-02-12 23:18:13 +0500

    crudCatDep

--- back/backoffice/crud-montant-depense/update-montant-depense.php
+++ back/backoffice/crud-montant-depense/update-montant-depense.php
@@ -15,6 +15,6 @@ else{
     $updated_montant = $_POST['montant'];
     $updated_date = $_POST['date'];
     
-    updateRecord('30h_montant_salaire', ['poid' => $updated_poid, 'montant' => $updated_montant, 'date' => $updated_date], 'id_montant_salaire = :id_montant_salaire', ['id_montant_salaire' => $id_montant_salaire_to_update]);
+    createRecord('30h_montant_salaire', ['poid' => $updated_poid, 'montant' => $updated_montant, 'date' => $updated_date], 'id_montant_salaire = :id_montant_salaire');
     }
 ?>
