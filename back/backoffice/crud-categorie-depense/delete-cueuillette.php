26a250d6225a5fb9ed63c66ef01ac3d5557e6dcd
benimaru73 <153352359+benimaru73@users.noreply.github.com>
2024-02-13 08:33:47 +0500

    categorie-depense

--- back/backoffice/crud-categorie-depense/delete-cueuillette.php
+++
@@ -1,6 +0,0 @@
-<?php
-include("function.php");
-
-$id_cat_dep_to_delete = $_POST['id_cat_dep'];
-deleteRecord('30h_categorie_depense', 'id_cat_dep = :id_cat_dep', ['id_cat_dep' => $id_cat_dep_to_delete]);
-?>
