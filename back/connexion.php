22429c76a1872154c7e7395e54e969f573260f5e
N1A1voIt <120253454+N1A1voIt@users.noreply.github.com>
2024-02-13 13:33:21 +0100

    ok

--- back/connexion.php
+++ back/connexion.php
@@ -1,9 +1,8 @@
 <?php
 function PDOConnect(){
-    $user = 'root';
-    $pass = 'root';
-    $dsn = 'mysql:host=localhost;port=3306;dbname=examfinals3';
-
+    $user = 'ETU002580';
+    $pass = 'XqKFM6BPElmM';
+    $dsn = 'mysql:host=172.10.0.113;port=3306;dbname=db_p16_ETU002580';
     try {
         $dbh = new PDO($dsn, $user, $pass);
         return $dbh;
